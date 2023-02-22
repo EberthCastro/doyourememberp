927cc96bea0e76f5a72984ce3d62185233595164
MariaManue02 <117834494+MariaManue02@users.noreply.github.com>
2023-02-22 20:21:49 +0100

    primer commit celestina

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import imagen from '../src/assets/imagenes/campoAtardecer.jpg'
+
+import './home.css'
+
+
+const Home = () => {
+  return (
+    <div className='featuredSearchContainer'>
+        <img src={imagen}/>
+    </div>
+  )
+}
+
+export default Home
