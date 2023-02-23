c3972cc8897c58263de7fc494de0931678ca610c
Little Z <zy_mz17@hotmail.es>
2023-02-23 09:37:47 +0100

    update colors

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,9 +1,6 @@
 
 
 import * as React from 'react';
-// import Box from '@mui/material/Box';
-// import TextField from '@mui/material/TextField';
-// import { CardMedia } from '@mui/material';
 import { Box, TextField, Typography, CardMedia, Button } from '@mui/material';
 
 export default function Form() {
@@ -15,7 +12,7 @@ export default function Form() {
         '& .MuiTextField-root': { m: 1 },
         
       }}
-      bgcolor='#ABABAB'
+      bgcolor='#F9B5AC'
       noValidate
       autoComplete="off"
     >
@@ -25,32 +22,32 @@ export default function Form() {
       }}>
         {/* <img src='../src/assets/Logo.png'></img> */}
 
-        <CardMedia
+        {/* <CardMedia
           component="img"
           sx={{ width: 120 }}
           image="/src/assets/Logo.png"
           alt="Live from space album cover"
-        />
-        <Typography variant="h5" component="h5" sx={{ color:'#656565', alignSelf:'center' }} > MORE INFORMATION </Typography>
+        /> */}
+        <Typography variant="h5" component="h5" sx={{ color:'#FFFFFF', alignSelf:'center' }} > MORE INFORMATION </Typography>
 
       </Box>
-      <TextField sx={{ width: '90%' }}
+      <TextField sx={{ width: '90%', borderRadius:'52px', bgcolor:'#FFFFFF', color:'#656565' }}
         required
-        id="outlined-required"
+        // id="outlined-required"
         label="Name"
       />
-      <TextField sx={{ width: '90%' }}
+      <TextField sx={{ width: '90%', borderRadius:'52px', bgcolor:'#FFFFFF', color:'#656565' }}
         required
-        id="outlined-required"
+        id="required"
         label="Phone"
       />
-      <TextField sx={{ width: '90%' }}
+      <TextField sx={{ width: '90%', borderRadius:'40px', bgcolor:'#FFFFFF', color:'#656565' }}
         id="outlined-multiline-static"
         label="Message"
         multiline
         rows={4}
       />
-      <Button variant="contained" disableElevation sx={{bgcolor:'white', color:'#656565', m:2}}>
+      <Button variant="contained" disableElevation sx={{bgcolor:'#FFFFFF', color:'#656565', m:2}}>
         SUBMIT
       </Button>
     </Box>
